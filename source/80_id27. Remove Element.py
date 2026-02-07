95306a3f9f0f919d70c26757e44c0f5cce396527
Nhat <minhnhatngo700@gmail.com>
2026-02-07 16:20:27 +0700

    update 8-

--- source/80_id27. Remove Element.py
+++ source/80_id27. Remove Element.py
@@ -1,9 +1,7 @@
 class Solution(object):
     def removeElement(self, nums, val):
-        nums.sort()
-        i = 1
-        
-        for j in range(1, len(nums)):
+        i = 0
+        for j in range(len(nums)):
             if nums[j] != val:
                 nums[i] = nums[j]
                 i += 1
